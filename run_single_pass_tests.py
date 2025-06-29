5d8a0a322d165006efe9c4a8589c7d225704f6f2
Federico Vancini <fevancin@gmail.com>
2025-06-29 16:35:33 +0200

    Fix

--- run_single_pass_tests.py
+++ run_single_pass_tests.py
@@ -124,7 +124,7 @@ for config_name, config_changes in configs['groups'].items():
     config = copy.deepcopy(configs['base'])
     for config_changes_key, config_changes_value in config_changes.items():
         if type(config_changes_value) is list:
-            if config_changes_value not in config:
+            if config_changes_key not in config:
                 config[config_changes_key] = []
             config[config_changes_key].extend(config_changes_value)
         else:
